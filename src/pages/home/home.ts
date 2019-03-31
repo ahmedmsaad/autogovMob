a6ef41f17a6a33fb175573e151b0e712daddfcf2
ahmedmsaad <ahmed.saad01011@gmail.com>
2019-04-01 00:31:26 +0200

    add new page (reqInfo)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ReqinfoPage} from '../reqinfo/reqinfo'; 
 @Component({
   selector: 'page-home',
@@ -7,10 +7,10 @@ import {ReqinfoPage} from '../reqinfo/reqinfo';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
   gotoReqInfo(){
+    console.log("done");
     this.navCtrl.push(ReqinfoPage)
   }
 }
