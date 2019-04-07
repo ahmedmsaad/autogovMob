4a6d05802c0e445d519e70c8865af440994c9f75
ahmedmsaad <ahmed.saad01011@gmail.com>
2019-04-07 19:11:42 +0200

    connet with backend using APIs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { IonicStorageModule } from '@ionic/storage';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -9,9 +11,10 @@ import { WelcomePage } from '../pages/welcome/welcome';
 import { LoginPage } from '../pages/login/login';
 import {ReqinfoPage} from '../pages/reqinfo/reqinfo'; 
 
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HttpModule } from '@angular/http'; 
 
 @NgModule({
   declarations: [
@@ -25,6 +28,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/reqinfo/reqinfo.ts
+++ src/pages/reqinfo/reqinfo.ts
@@ -7,18 +7,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+import {apiKey} from "../../app/apiurls/serverurls.js";
+import { Http , Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Storage } from '@ionic/storage';
+
+
+
 @IonicPage()
 @Component({
   selector: 'page-reqinfo',
   templateUrl: 'reqinfo.html',
 })
 export class ReqinfoPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  mydata:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams ,public storage: Storage , public http: Http) {
+    this.getAllDocuments().then((data) => {
+      this.mydata = data[1];
+      console.log(this.mydata)
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReqinfoPage');
   }
-
+  getAllDocuments(){
+      return new Promise((resolve, reject) => {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+ 
+        this.http.get(apiKey+'api/documents/get', {headers: headers})
+         .map(res => res.json())
+         .subscribe(data => {
+           resolve(data);
+         }, (err) => {
+           reject(err);
+         }); 
+      })
+  }
 }
