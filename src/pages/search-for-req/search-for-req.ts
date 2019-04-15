1a3105caff61e363cbeb69f61951a9196eaf08cf
ahmedmsaad <ahmed.saad01011@gmail.com>
2019-04-15 18:59:59 +0200

    Search for Instance request by instanceID and get customer & transaction details

--- src/pages/search-for-req/search-for-req.ts
+++ src/pages/search-for-req/search-for-req.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
+import { apiKey } from "../../app/apiurls/serverurls.js";
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Storage } from '@ionic/storage';
+import { resolveDefinition } from '@angular/core/src/view/util';
+import {WelcomePage} from "../welcome/welcome"
+import { Title } from '@angular/platform-browser';
 /**
  * Generated class for the SearchForReqPage page.
  *
@@ -14,12 +20,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'search-for-req.html',
 })
 export class SearchForReqPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  InstanceId: any;
+  customer:any;
+  transaction:any;
+  name:string=" ";
+  constructor(public alertCtrl: AlertController,public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public http: Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchForReqPage');
   }
+  showResult(){
+    this.getTrancationAndCustomer().then((data)=>{
+      this.customer=data[0];
+      this.transaction=data[1];
+      console.log(this.customer)
+      
+    })
+  }
+  getTrancationAndCustomer() {
+    return new Promise((resolve, reject) => {
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
 
+      this.http.post(apiKey + 'api/transactions/fetchSec', { Instance_id: this.InstanceId })
+        .map(res => res.json())
+        .subscribe(data => {
+          console.log(data)
+          resolve(data);
+        }, (err) => {
+          reject(err);
+        });
+    })
+  }
 }
